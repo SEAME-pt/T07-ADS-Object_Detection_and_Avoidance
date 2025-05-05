52c7a7b99a2cf24f9ccb96ff261b956f5cfbd1fc
afmyhouse <af.myhouse@gmail.com>
2025-05-05 16:36:16 +0100

    feature:mpc > issue:autotune inner pid loop

--- speedController/mainPIDautoTuner.cpp
+++ speedController/mainPIDautoTuner.cpp
@@ -2,23 +2,26 @@
 #include "SpeedPIDController.hpp"
 #include "SpeedPIDTuner.hpp"
 
+const float PWM_MIN = 0.0f; // Minimum PWM value
+const float PWM_MAX = 100.0f; // Maximum PWM value
+
 int main() {
-	SpeedPIDController pid(0.5f, 0.1f, 0.05f, 0.0f, 100.0f);
 	float dt = 0.1f; // Time step
 	float sim_time = 10.0f; // Simulation time
 	float v_target = 2.0f; // Target speed
 	float v_current = 0.0f; // Initial speed
-	float pwm_input = 0.0f; // Initial PWM input
-	float total_error = 0.0f;
-	float max_overshoot = 0.0f;
-	float final_error = 0.0f;
+	float pwm_input = PWM_MIN; // Initial PWM input
 
-    auto [kp, ki, kd] = auto_tune_pid();
+	auto [kp, ki, kd] = auto_tune_pid(dt, sim_time, v_target);;
 
     std::cout << "Best PID gains found:\n";
     std::cout << "Kp = " << kp << "\n";
     std::cout << "Ki = " << ki << "\n";
     std::cout << "Kd = " << kd << "\n";
-
+	std::cout << "Simulating with tuned PID controller...\n";
+	SpeedPIDController traction(kp, ki, kd, PWM_MIN, PWM_MAX);
+	traction.reset();
+	v_current = 0.0f;//go fetch current speed from ZMQ
+	
     return 0;
 }
